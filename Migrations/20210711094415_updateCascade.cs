b05c77360b05d45dc3023270bd6c189533343c59
Niko D <nikodojan@live.de>
2021-07-11 14:27:18 +0200

    menu service context

---
+++ Migrations/20210711094415_updateCascade.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorRecipeApp.Migrations
+{
+    public partial class updateCascade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Day_Menu_MenuId",
+                table: "Day");
+            migrationBuilder.AddForeignKey(
+                name: "FK_Day_Menu_MenuId",
+                table: "Day",
+                column: "MenuId",
+                principalTable: "Menu",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade,
+                onUpdate: ReferentialAction.Cascade);
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meal_Day_DayId",
+                table: "Meal");
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meal_Day_DayId",
+                table: "Meal",
+                column: "DayId",
+                principalTable: "Day",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade,
+                onUpdate: ReferentialAction.Cascade);
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dish_Meal_MealId",
+                table: "Dish");
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dish_Meal_MealId",
+                table: "Dish",
+                column: "MealId",
+                principalTable: "Meal",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade,
+                onUpdate: ReferentialAction.Cascade);
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Day_Menu_MenuId",
+                table: "Day");
+            migrationBuilder.AddForeignKey(
+                name: "FK_Day_Menu_MenuId",
+                table: "Day",
+                column: "MenuId",
+                principalTable: "Menu",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meal_Day_DayId",
+                table: "Meal");
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meal_Day_DayId",
+                table: "Meal",
+                column: "DayId",
+                principalTable: "Day",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dish_Meal_MealId",
+                table: "Dish");
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dish_Meal_MealId",
+                table: "Dish",
+                column: "MealId",
+                principalTable: "Meal",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Services/Services/EfMenuServiceV1.cs
+++ Services/Services/EfMenuServiceV1.cs
@@ -10,64 +10,50 @@ namespace BlazorRecipeApp.Services.Services
     public class EfMenuServiceV1 : IMenuService
     {
         private IDbContextFactory<ApplicationDbContext> _factory;
+        private readonly ApplicationDbContext _context;
 
         public EfMenuServiceV1(IDbContextFactory<ApplicationDbContext> factory)
         {
             _factory = factory;
+            _context = _factory.CreateDbContext();
         }
 
         public async Task<IEnumerable<Menu>> GetAllMenusAsync()
         {
-            using (var ctx = _factory.CreateDbContext())
-            {
-                return await ctx.Menus
-                    .Include(menu=>menu.Days)
-                    .ThenInclude(day=>day.Meals)
-                    .ThenInclude(meal=>meal.Dishes)
-                    .ThenInclude(dish=>dish.Recipe)
-                    .ToListAsync();
-            }
+            return await _context.Menus
+                .Include(menu=>menu.Days)
+                .ThenInclude(day=>day.Meals)
+                .ThenInclude(meal=>meal.Dishes)
+                .ThenInclude(dish=>dish.Recipe)
+                .ToListAsync();
         }
 
         public async Task CreateMenuAsync(Menu menu)
         {
-            using var ctx = _factory.CreateDbContext();
-            {
-                await ctx.Menus.AddAsync(menu);
-                await ctx.SaveChangesAsync();
-            }
+            await _context.Menus.AddAsync(menu);
+            await _context.SaveChangesAsync();
         }
 
         public async Task<Menu> GetMenuByIdAsync(int menuId)
         {
-            using (var ctx = _factory.CreateDbContext())
-            {
-                return await ctx.Menus
-                    .Include(menu => menu.Days)
-                    .ThenInclude(day => day.Meals)
-                    .ThenInclude(meal => meal.Dishes)
-                    .ThenInclude(dish => dish.Recipe)
-                    .FirstOrDefaultAsync(m=>m.Id == menuId);
-            }
+            return await _context.Menus
+                .Include(menu => menu.Days)
+                .ThenInclude(day => day.Meals)
+                .ThenInclude(meal => meal.Dishes)
+                .ThenInclude(dish => dish.Recipe)
+                .FirstOrDefaultAsync(m=>m.Id == menuId);
         }
 
         public async Task UpdateMenuAsync(Menu menu)
         {
-            using (var ctx = _factory.CreateDbContext())
-            {
-                ctx.Menus.Update(menu);
-                await ctx.SaveChangesAsync();
-            }
-            
+            _context.Menus.Update(menu);
+            await _context.SaveChangesAsync();
         }
 
         public async Task DeleteMenuAsync(Menu menu)
         {
-            using (var ctx = _factory.CreateDbContext())
-            {
-                ctx.Menus.Remove(menu);
-                await ctx.SaveChangesAsync();
-            }
+            _context.Menus.Remove(menu);
+            await _context.SaveChangesAsync();
         }
     }
 }
